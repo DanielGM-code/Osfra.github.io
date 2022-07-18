c32772edf52c988489f45fd0734bb56ea29a9c6b
Daniel Morales <62160774+DanielGM-code@users.noreply.github.com>
2022-07-18 10:15:33 -0500

    Add files via upload

---
+++ js/formulario.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('botonEnviar');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_dbhweqq';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Enviar';
+      alert('Enviado!');
+    }, (err) => {
+      btn.value = 'Enviar';
+      alert(JSON.stringify(err));
+    });
+});
